be0f014d5995726e9d950c3e1f5dba4bb9c565c9
TedyAditiya <117249648+tedyadt@users.noreply.github.com>
2025-11-28 09:46:52 +0700

    Initial commit - My portfolio

---
+++ components/about/More.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function More() {
+    return (
+      <></>
+    );
+}
+
+export default More
---
+++ data/content/home.ts
@@ -0,0 +1,84 @@
+type Skill = {
+  title: string,
+  icon: string,
+  style?: object
+};
+type Testimonial = {
+  quote: string,
+  name: string,
+  job: string
+};
+
+
+export const skills: Skill[] = [
+  {
+    title: "HTML",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+  },
+  {
+    title: "CSS",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+  },
+  {
+    title: "Javascript",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+  },
+  {
+    title: "PostgreSQL",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original.svg"
+  },
+  {
+    title: "TailwindCSS",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original-wordmark.svg",
+  },
+  {
+    title: "React JS",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+  },
+  {
+    title: "React Native",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+    
+  },
+  {
+    title: "Laravel",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/laravel/laravel-original.svg",
+  },
+  {
+    title: "NodeJS",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+  },
+  {
+    title: "Git",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
+  },
+  {
+    title: "AWS Codecommit",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/amazonwebservices/amazonwebservices-original-wordmark.svg",
+  },
+  {
+    title: "Figma",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg",
+  },
+];
+
+export const testimonials: Testimonial[] = [
+  {
+    quote:
+      "The communication throughout the project was spot on and the genre of design was carried out well.",
+    name: "Wilson Bright",
+    job: "Co-Founder of BlockSurvey",
+  },
+  {
+    quote:
+      "The code was clean, easy to understand, and exactly what I was looking for.",
+    name: "Philip Imperato",
+    job: "CEO of VisualBonus",
+  },
+  {
+    quote:
+      "My vision was executed perfectly. The quality and speed of the work was excellent.",
+    name: "Zach Green",
+    job: "Founder of Hangxiety Shrink",
+  },
+];
